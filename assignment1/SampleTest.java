cde37d0bffc58803153ee8e71677e1184fb7626a
alanpeni <Alan_penichet-paul@selinc.com>
2019-01-31 15:31:17 +1100

    finished prolly

--- assignment1/SampleTest.java
+++ assignment1/SampleTest.java
@@ -3,6 +3,9 @@ package assignment1;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+import java.util.Random;
+
 import org.junit.Test;
 
 
@@ -59,10 +62,75 @@ public class SampleTest {
 	}
 	@Test(timeout = 2000)
 	public void testInsertPlaceMiddle(){
-		int[] x = new int[]{1, 2, 23 ,54 ,68};
+		//fails with out of bound if cant reach n+1
+		int[] x = new int[]{1, 2, 23 ,54 ,68, 71};
 		int[] expected = new int[]{1, 2, 3, 23, 54, 68};
 		assertEquals(6, SortTools.insertInPlace(x, 5, 3));
-		assertArrayEquals(x, expected);
+		assertArrayEquals(expected, x);
 	}
-	
+	@Test(timeout = 2000)
+	public void testInsertPlaceFound(){
+		int[] x = new int[]{1, 2, 3 ,23 ,54};
+		int[] expected = new int[]{1, 2, 3, 23, 54};
+		assertEquals(4, SortTools.insertInPlace(x, 4, 3));
+		assertArrayEquals(expected, x);
+	}
+	@Test(timeout = 2000)
+	public void testInsertPlacesmall(){
+		int[] x = new int[]{1, 10};
+		int[] expected = new int[]{1, 2};
+		assertEquals(2, SortTools.insertInPlace(x, 1, 2));
+		assertArrayEquals(expected, x);
+	}
+	@Test(timeout = 2000)
+	public void testInsertPlaceLast(){
+		int[] x = new int[]{1,2,3,4,5, 10};
+		int[] expected = new int[]{1, 2,3,4,5,6};
+		assertEquals(6, SortTools.insertInPlace(x, 5, 6));
+		assertArrayEquals(expected, x);
+	}
+	@Test(timeout = 2000)
+	public void testInsertSortGeneral(){
+		int[] x = new int[]{7, 5, 1, 8, 3, 2, 6};
+		int[] expected = new int[]{1, 2, 3, 5, 6, 7, 8};
+		SortTools.insertSort(x, 7);
+		assertArrayEquals(expected, x);
+	}
+	@Test(timeout = 2000)
+	public void testInsertSortRandom() {
+		//creates random numbers
+		Random random= new Random();
+		int length = random.nextInt(1000);
+        int numbers[]= new int[length];
+        for (int i = 0; i < length; i++) 
+        {
+        int number= random.nextInt(100);
+        numbers[i]=number;
+        }
+        int copy[] = Arrays.copyOf(numbers, length);
+        Arrays.sort(copy);
+        SortTools.insertSort(numbers, length);
+        assertArrayEquals(copy, numbers);
+	}
+	@Test(timeout = 100)
+	public void testInsertSortSimple(){
+		int length = 200000;
+		int[] x = new int[length];
+		for(int i=0; i < length; i++) {
+			x[i] = i;
+		}
+		int[] expected = Arrays.copyOf(x, length);
+		int temp = x[(length/2)];
+		x[(length/2)] = x[(length/2)+1];
+		x[(length/2)+1] = temp;
+		SortTools.insertSort(x, length);
+		assertArrayEquals(expected, x);
+	}
+	@Test(timeout = 2000)
+	public void testInsertSortOne(){
+		int[] x = new int[]{8, 4, 3, 6};
+		int[] expected = new int[]{8, 4, 3, 6};
+		SortTools.insertSort(x, 1);
+		assertArrayEquals(expected, x);
+	}
 }
--- assignment1/SortTools.java
+++ assignment1/SortTools.java
@@ -21,9 +21,11 @@ public class SortTools {
 	  */
 	public static boolean isSorted(int[] x, int n) {
 		if(n==0 || x.length == 0) {
+			//special cases
 			return false;
 		}
 		for(int i = 1; i < n; i++) {
+			//return false if not sorted at any point
 			if(x[i-1] > x[i]) {
 				return false;
 			}
@@ -39,22 +41,29 @@ public class SortTools {
 	 * @return index of v if v is contained within x. -1 if v is not contained within x
 	 */
 	public static int find(int[] x, int n, int v) {
+		//set bounds to initial values
 		int left  = 0;
 		int right = n-1;
+		//keep looping while bounds dont swap, indicating end.
 		while(left <= right) {
+			//pivot
 			int middle = (left+right)/2;
 			if(left == right) {
+				//if bounds same and not found, value not in array. return -1
 				if(x[middle]!=v) {
 					return -1;
 				}
 			}
 			if(x[middle] == v) {
+				//value found
 				return middle;
 			}
 			if(x[middle] > v) {
+				//keep left half
 				right = middle;
 			}
 			if(x[middle] < v) {
+				//keep right half
 				left = middle;
 			}
 		}
@@ -69,35 +78,35 @@ public class SortTools {
 	 * @param v is the value to be added to the new array
 	 * @return a new array containing the first n elements of x, and v
 	 */
-	public static int[] insertGeneral(int[] orig, int len, int val){
-		int[] temp = new int[len+1];
+	public static int[] insertGeneral(int[] x, int n, int v){
+		int[] temp = new int[n+1];
 		//flag indicates value was inserted.
 		boolean flag = false;
-		if(len == 0) {
+		if(n == 0) {
 			return new int[] {};
 		}
-		for(int i = 0; i < len ; i++) {
+		for(int i = 0; i < n ; i++) {
 			if(!flag) {
 				//if val not placed yet, continue normally
-				if(orig[i] < val) {
-					temp[i] = orig[i];
+				if(x[i] < v) {
+					temp[i] = x[i];
 					//if still not found and at end, append.
-					if(i == len-1) {
-						temp[i+1] = val;
+					if(i == n-1) {
+						temp[i+1] = v;
 					}
 				}
-				if(orig[i] == val) {
+				if(x[i] == v) {
 					//if value found, return truncated array.
-					return Arrays.copyOf(orig, len);
+					return Arrays.copyOf(x, n);
 				}
-				if(orig[i] > val) {
-					temp [i] = val;
+				if(x[i] > v) {
+					temp [i] = v;
 					flag = true;
 				}
 			}
 			//if val found, offset by one.
 			if(flag) {
-				temp[i+1] = orig[i];
+				temp[i+1] = x[i];
 			}
 		}
         return temp;
@@ -111,32 +120,44 @@ public class SortTools {
 	 * @return n if v is already in x, otherwise returns n+1
 	 */
 	public static int insertInPlace(int[] x, int n, int v){
-		//doesn't actually change x rn?
-		//try to copy x to temp at start, then modify x from temp one at a time
-		//try reinitializing x?
-		boolean flag = false;
+		boolean missing = false;
 		int[] temp = Arrays.copyOf(x, n+1);
-		for(int i = 1; i < n; i++) {
-			//if value found, just return n and don't modify x
-			if(temp[i] == v) {
-				return n;
-			}
-			if(!flag)
-			{
-				// keep same as long as value isnt missing
-				if(temp[i] > v && temp[i-1] < v)
+		//first position case
+		if(temp[0]>v) {
+			x[0] = v;
+			missing = true;
+		}
+		for(int i = 0; i < n; i++) {
+				if(!missing)
 				{
-					//if value missing, set flag to true and temp i to value
-					x[i] = v;
-					flag = true;
+					//if value found, just return n and don't modify x
+					if(temp[i] == v)
+					{
+						return n;
+					}
+					// keep same as long as value isnt missing, avoid out of bounds
+					if(i>0) 
+					{
+						if(temp[i] > v && temp[i-1] < v)
+						{
+							//if value missing, set flag to true and temp i to value
+							x[i] = v;
+							missing = true;
+						}
+					}
 					
 				}
-			}
-			if(flag)
-			{
-				//once missing, increment by one
-				x[i+1] = temp[i];
-			}
+			
+				if(missing)
+				{
+					//once missing, increment by one
+					x[i+1] = temp[i];
+				}
+		}
+		if(!missing)
+		{
+			//if still not missing by end, just append.
+			x[n] = v;
 		}
 		return n+1;
     }
@@ -147,5 +168,24 @@ public class SortTools {
 	 * @param n is the number of elements of the array to be sorted
 	 */
 	public static void insertSort(int[] x, int n){
+		//flag for O(n) cases
+		boolean sorted = true;
+		for(int i =0; i < n; i++) {
+			//reset to sorted to see if any are wrong
+			sorted = true;
+			//assumed top i values are sorted correctly
+			for(int j = 0; j < n-i-1; j++) {
+				if(x[j] > x[j+1]) {
+					//indicate array was wrong, try next loop
+					sorted = false;
+					//sort 2 at a time
+					int temp = x[j];
+					x[j] = x[j+1];
+					x[j+1] = temp;
+				}
+			}
+			//if sorted, skip remaining loops
+			if(sorted) return;
+		}
     }
 }
